fa38f699b67c31f29268548a1484437c939d082c
AlexLupo5 <123680387+AlexLupo5@users.noreply.github.com>
2023-11-24 22:44:58 -0500

    Update convexHull.h

--- convexHull.h
+++ convexHull.h
@@ -9,20 +9,24 @@ class convexHull {
 
 private:
     std::string fileName;
-std::vector <int> xCoordinates;
-std::vector <int> yCoordinates;
+    std::vector <int> xCoordinates;
+    std::vector <int> yCoordinates;
+    int lowX = 0;
+    int lowY = 0;
+    std::vector <std::string> output;
 
 public:
 
     convexHull(std::string fileName); //convexHull constructor
-
     ~convexHull(); //convexHull deconstructor
 
     void convertCordToVect();
+    int findLowestYCoordPair();
 
-    void findLowestY();
+    void addToOutput(int i);
+    void printOutput();
 
-    double polarAngle(int x, int y); //Returns the polarAngle of the given (x,y) coordinates
+    double polarAngle(int x, int y); //Returns the polarAngle of the given (x2- x1,y2- y1) coordinates
 
 };
 
