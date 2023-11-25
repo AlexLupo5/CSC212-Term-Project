ea36fdd0c98d4b42e0ca572454a90c0484e621ce
JakeN237 <123680490+JakeN237@users.noreply.github.com>
2023-11-25 10:32:04 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include "Project.h"
 
-int main(){
+int main() {
+    //Get points from the file
+    std::ifstream inputFile("points.txt");
+    if (!inputFile.is_open()) {
+        std::cout << "Error opening file";
+        std::cout << "\n";
+        return 1;
+    }
 
+    //Insert the number of points
+    int Points;
+    inputFile >> Points;
+
+    //Save the points in the vector
+    std::vector<Point> pointsvect(Points);
+    for (int i = 0; i < Points; i++) {
+        inputFile >> pointsvect[i].x >> pointsvect[i].y;
+    }
+
+    inputFile.close();
+
+    //Do the Convex hull
+    std::vector<Point> hull = convexHull(points);
+    return 0;
 }
