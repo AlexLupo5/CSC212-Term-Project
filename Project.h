9e133d8f3a19f432cd95c89fc328930683526a49
JakeN237 <123680490+JakeN237@users.noreply.github.com>
2023-11-28 12:58:11 -0500

    Update Project.h

--- Project.h
+++ Project.h
@@ -2,26 +2,24 @@
 #include <vector>
 
 class Point{
-private:
-  int x;
-  int y;
-
 public:
-  Point(int x, int y);
-  ~Point();
-}
+    Point(int x, int y) : x(x), y(y) {};
+    ~Point();
+    int x;
+    int y;
+};
 
 class Convex{
 private:
-  Point temp;
-  std::vector<Point> Point;
-  std::vector<Point> ConHull;
+    Point temp;
+    std::vector<Point> Pvect;
+    std::vector<Point> ConHull;
 
 public:
-  Convex(std::vector<Point> pointsvect);
-  ~Convex();
-  int orientation(Point a, Point b, Point c);
-  void computeConvex();
-  std::vector<Point> getConvex();
-  bool compare(Point a, Point b);
+    Convex(std::vector<Point> pointsvect);
+    ~Convex();
+    void computeConvex();
+    std::vector<Point> getConvex();
+    int orientation(std::vector<Point> a, std::vector<Point> b, std::vector<Point> c);
+    bool compare(std::vector<Point> a, std::vector<Point> b);
 };
