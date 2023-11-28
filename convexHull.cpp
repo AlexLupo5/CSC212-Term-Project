9f1158758567ee4966b78bad7d756651b69d46a0
JakeN237 <123680490+JakeN237@users.noreply.github.com>
2023-11-28 13:56:08 -0500

    Update convexHull.cpp

--- convexHull.cpp
+++ convexHull.cpp
@@ -189,3 +189,16 @@ void convexHull::convertCoordToXYVect() {
     return;
 
 }
+
+void convexHull::Visualize(){
+    std::ofstream dotfile(fileName + ".dot");
+    if (dotfile.is_open()){
+        dotfile << "graph convexHull";
+        dotfile << "{\n";
+        for(int i = 0; i < output.size(); i++) {
+            dotfile << output[i] << ";\n";
+        }
+        dotfile << "}\n";
+        dotfile.close();
+    }
+}
