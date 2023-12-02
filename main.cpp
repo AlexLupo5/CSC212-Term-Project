9f23daedec71c0dc261625ff09f3aa08f980a35f
AlexLupo5 <123680387+AlexLupo5@users.noreply.github.com>
2023-12-02 17:37:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
+#include "convexHull.h"
+#include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <cmath>
+
 
 /*input file should be formatted as shown below
  *(x1,y1)
@@ -11,9 +15,8 @@
 int main(int argc, char*argv[]) {
 
     convexHull shape((std::string(argv[1]))); //Creates an object of convexHull, in provided main.cpp object is called shape
-    shape.print(); //Prints the convexHull output to terminal
-    shape.dot(); //Prints the convexHull output to a .dot file
-    shape.dot2();
+shape.print(); //Prints the convexHull output to terminal
+shape.dot(); //Prints the convexHull output to a .dot file
 
     return 0;
 }
