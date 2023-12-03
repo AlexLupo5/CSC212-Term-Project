7d33d58a96afd12a442f70cdb1da978c09c91d0e
AlexLupo5 <123680387+AlexLupo5@users.noreply.github.com>
2023-12-03 14:59:19 -0500

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,1 +1,56 @@
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
 
+class graph {
+
+private:
+    std::string filename1;
+    std::string filename2;
+    std::string filename3;
+
+    int shapeCount;
+
+    std::vector<sf::Vector2f> greenPoints; //Shape 1
+    std::vector<sf::Vector2f> bluePoints; //Shape 2
+    std::vector<sf::Vector2f> redPoints; //Shape 3
+
+    double minX = 0;
+    double minY = 0;
+    double maxX = 0;
+    double maxY = 0;
+
+    sf::ConvexShape greenShape;
+    std::vector<sf::CircleShape> greenCircles;
+
+    sf::ConvexShape blueShape;
+    std::vector<sf::CircleShape> blueCircles;
+
+    sf::ConvexShape redShape;
+    std::vector<sf::CircleShape> redCircles;
+
+    std::vector<sf::Vertex> greenLines;
+    std::vector<sf::Vertex> blueLines;
+    std::vector<sf::Vertex> redLines;
+
+public:
+    graph(std::string filename, int shapeCount);
+    graph(std::string filename, std::string filenameBlue, int shapeCount);
+    graph(std::string filename, std::string filenameBlue, std::string filenameRed, int shapeCount);
+
+    void sizeWindow();
+    void createShapes();
+    void setColors();
+    void createLines();
+    void plot();
+
+    ~graph();
+
+};
+
+#endif
