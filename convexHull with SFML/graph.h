05a8f254a869ba48f03ab4875d98ba9d3a2da0b8
AlexLupo5 <123680387+AlexLupo5@users.noreply.github.com>
2023-12-04 20:06:04 -0500

    Update graph.h

--- convexHull with SFML/graph.h
+++ convexHull with SFML/graph.h
@@ -10,45 +10,38 @@
 class graph {
 
 private:
-    std::string filename1;
+    std::string filename1; //Create three strings that hold the names of three filenames
     std::string filename2;
     std::string filename3;
 
-    int shapeCount;
+    int shapeCount; //Int that holds the amount of shapes being graphed
 
     std::vector<sf::Vector2f> greenPoints; //Shape 1
     std::vector<sf::Vector2f> bluePoints; //Shape 2
     std::vector<sf::Vector2f> redPoints; //Shape 3
 
-    double minX = MAXFLOAT;
-    double minY = MAXFLOAT;
-    double maxX = -MAXFLOAT;
-    double maxY = -MAXFLOAT;
+    double minX = MAXFLOAT; //Holds the max float value for a more accurate comparison between minX points
+    double minY = MAXFLOAT; //Holds the max float value for a more accurate comparison between minY points
+    double maxX = -MAXFLOAT; //Holds the negative max float value for a more accurate comparison between maxX points
+    double maxY = -MAXFLOAT; //Holds the negative max float value for a more accurate comparison between maxY points
 
-    sf::ConvexShape greenShape;
-    std::vector<sf::CircleShape> greenCircles;
+    sf::ConvexShape greenShape; //Create the first shape named greenShape
 
-    sf::ConvexShape blueShape;
-    std::vector<sf::CircleShape> blueCircles;
+    sf::ConvexShape blueShape; //Create the second shape named blueShape
 
-    sf::ConvexShape redShape;
-    std::vector<sf::CircleShape> redCircles;
-
-    std::vector<sf::Vertex> greenLines;
-    std::vector<sf::Vertex> blueLines;
-    std::vector<sf::Vertex> redLines;
+    sf::ConvexShape redShape; //Create the third shape named redShape
 
 public:
-    graph(std::string filename, int shapeCount);
-    graph(std::string filename, std::string filenameBlue, int shapeCount);
+    graph(std::string filename, int shapeCount); //Constructor for one shape
+    graph(std::string filename, std::string filenameBlue, int shapeCount); //Constructor for second shape
     graph(std::string filename, std::string filenameBlue, std::string filenameRed, int shapeCount);
 
-    void sizeWindow();
-    void createShapes();
-    void setColors();
-    void plot();
+    ~graph(); //graph deconstructor
 
-    ~graph();
+    void sizeWindow(); //sizeWindow function
+    void createShapes(); //createShapes function
+    void setColors(); //setColors function
+    void plot(); //plot function
 
 };
 
