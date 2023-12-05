019feb925c7bc432c821c175818a7b6365e71091
AlexLupo5 <123680387+AlexLupo5@users.noreply.github.com>
2023-12-04 19:54:50 -0500

    Update convexHull.h

--- convexHull/convexHull.h
+++ convexHull/convexHull.h
@@ -6,7 +6,7 @@
 #include <sstream>
 #include <cmath>
 
-struct Point {
+struct Point { //Used so we can work with points instead of multiple x and y arrays and vectors
     double x, y;
 };
 class convexHull {
